83dcd5346c5430e4a35bbd158732a3f00610bc30
ashleynoriega <chapina.ashley@gmail.com>
2019-09-23 22:03:04 -0700

    Adding circumference file

---
+++ circumference.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import math
+
+# pi = math.pi
+pi = 3.14159
+
+radius = 3
+calculation = pi * radius * 2.0
+
+print("The circumference of a circle of radius 3 is " + str(calculation))
